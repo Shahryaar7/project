874859c729defdb769ddae889227ad024ea832f6
Shahryaar7 <shahryar71@gmail.com>
2023-01-30 02:29:22 +0330

    scope added

--- tutorial/app/models/cafe_restaurant.rb
+++ tutorial/app/models/cafe_restaurant.rb
@@ -1,3 +1,8 @@
 class CafeRestaurant < ApplicationRecord
+  has_many :user_roles
+  has_many :users, through: :user_roles
+
   validates :name, presence: true
+
+  scope :by_names, ->(letters) { where("name ILIKE ?","%#{letters}%") }
 end
--- tutorial/app/models/user.rb
+++ tutorial/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_many :user_roles
+  has_many :cafe_restaurants, through: :user_roles
+
   validates :name, presence: true
+
+  scope :by_names, ->(letters) { where("name ILIKE ?","%#{letters}%") }
 end
